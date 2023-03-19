9f2a4e91a23554cd3ae343653865cccb0ffe2506
lexzor <super.alexx@yahoo.com>
2023-03-19 21:24:17 +0200

    for vps3

--- backend/dbConn.php
+++ backend/dbConn.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect("185.234.69.234", "carlottery", "gwug4JJAhyFwBVzY", "loterie");
+$db = mysqli_connect("185.234.69.234", "carlottery", "gwug4JJAhyFwBVzY", "carloterry");
 
 if (!$db) {
     print('Conexiunea la baza de date a esuat!');
