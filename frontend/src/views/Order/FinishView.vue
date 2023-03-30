eddabb1c9691f4f7585c9985a85a567219edcb69
lexzor <super.alexx@yahoo.com>
2023-03-30 18:51:01 +0300

    ---

--- frontend/src/views/Order/FinishView.vue
+++ frontend/src/views/Order/FinishView.vue
@@ -2,7 +2,34 @@
 import NavBar from '../../components/NavBar.vue';
 import MazInput from 'maz-ui/components/MazInput'
 import MazSelect from "maz-ui/components/MazSelect"
-import MazTextarea from "maz-ui/components/MazTextarea"
+import MazTextArea from "maz-ui/components/MazTextArea"
+
+import { reactive } from 'vue';
+import useVuelidate from '@vuelidate/core';
+import { required, email } from '@vuelidate/validators'
+import { vuelidateTranslator } from '../../additional/translator';
+
+const countrys = [
+    { label: 'Romania', value: 'Romania' },
+    { label: 'Franta', value: 'Franta' },
+    { label: 'Italia', value: 'Italia' },
+    { label: 'Spania', value: 'Spania' },
+    { label: 'Irlanda', value: 'Irlanda' },
+    { label: 'Germania', value: 'Germania' },
+    { label: 'Belgia', value: 'Belgia' },
+    { label: 'Austria', value: 'Austria' },
+    { label: 'Grecia', value: 'Grecia' },
+    { label: 'Regatul unit', value: 'Regatul Unit' },
+    { label: 'Republica Moldova', value: 'Republica Moldova' },
+    { label: 'Israel', value: 'Israel' },
+    { label: 'Ungaria', value: 'Ungaria' },
+    { label: 'Olanda', value: 'Olanda' },
+    { label: 'Danemarca', value: 'Danemarca' },
+    { label: 'Suedia', value: 'Suedia' },
+    { label: 'Portugalia', value: 'Portugalia' },
+    { label: 'Serbia', value: 'Serbia' },
+    { label: 'Irlanda', value: 'Irlanda' },
+]
 
 /*
     await axios.post('http://localhost/loterie/makePayment.php', {
@@ -17,6 +44,36 @@ import MazTextarea from "maz-ui/components/MazTextarea"
     })
 */
 
+const state = reactive({
+    firstName: '',
+    lastName: '',
+    companyName: '',
+    country: '',
+    address: '',
+    secondAddress: '',
+    zipCode: '',
+    city: '',
+    phone: '',
+    email: '',
+    additionalInformation: '',
+})
+
+const rules = {
+    firstName: { required },
+    lastName: { required },
+    companyName: { required },
+    country: { required },
+    address: { required },
+    secondAddress: { required },
+    zipCode: { required },
+    city: { required },
+    phone: { required },
+    email: { required, email },
+    additionalInformation: { required },
+}
+
+const v = useVuelidate(rules, state)
+
 </script>
 <template>
         <NavBar />
@@ -28,6 +85,7 @@ import MazTextarea from "maz-ui/components/MazTextarea"
             <MazInput required class="w-full" label="Nume" no-radius />    
         </div>
         <MazInput no-radius label="Nume Companie (Optional)" />
+        <MazSelect required label="Tara" no-radius :options="countrys" search max-list-width="full" />
         <MazInput required no-radius label="Nume strada, numar etc." />
         <MazInput no-radius label="Apartament, complex, unitate etc. (optional)" />
         <MazInput required no-radius label="Cod Postal" />
@@ -35,6 +93,28 @@ import MazTextarea from "maz-ui/components/MazTextarea"
         <MazInput required no-radius label="Telefon" />
         <MazInput required no-radius label="Adresa email" />
         <h1 class="uppercase text-center text-[20px] font-bold">Informatii Suplimentare</h1>
-        <MazTextarea required no-radius label="Note comanda (optional)" id="15" placeholder="Note referitoare la comanda ta, de exemplu: anumite note pentru livrare" />
+        <MazTextArea required no-radius label="Note comanda (optional)" placeholder="Note referitoare la comanda ta, de exemplu: anumite note pentru livrare" />
+        <h1 class="uppercase text-center text-[20px] font-bold">Comanda Ta</h1>
+        <div class="w-full flex flex-col border-[1px] border-b-gray-300">
+            <div class="flex justify-between border-b-[1px] border-b-gray-300 p-[20px]">
+                <h1 class="font-bold">Produs</h1>
+                <h1 class="font-bold">Sub-total</h1>
+            </div>
+            <div class="flex items-center justify-between border-b-[1px] border-b-gray-300 p-[20px]">
+                <div>
+                    <h1>Mercedes GLA <span class="font-bold">x 5</span></h1>
+                    <h1><span class="font-bold">Raspuns:</span> C220</h1>
+                </div>
+                <h1>15&euro;</h1>
+            </div>
+            <div class="flex justify-between border-b-[1px] border-b-gray-300 p-[20px]">
+                <h1 class="font-bold">Sub-total</h1>
+                <h1 class="font-bold">15&euro;</h1>
+            </div>
+            <div class="flex justify-between p-[20px]">
+                <h1 class="font-bold">Produs</h1>
+                <h1 class="font-bold">Sub-total</h1>
+            </div>
+        </div>
     </div>
 </template>
