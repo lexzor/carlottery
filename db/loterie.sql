e8559e93c570a1e78002b378a7417aba77c99bf9
Pavel Marin <kraiermarin11@gmail.com>
2023-04-02 16:18:52 +0100

    Update loterie.sql

--- db/loterie.sql
+++ db/loterie.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 28, 2023 at 11:14 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Apr 02, 2023 at 05:18 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,14 +35,14 @@ CREATE TABLE `accounts` (
   `access` int(11) NOT NULL DEFAULT 0,
   `login_key` varchar(33) NOT NULL,
   `cart` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `accounts`
 --
 
 INSERT INTO `accounts` (`id`, `email`, `upassword`, `username`, `access`, `login_key`, `cart`) VALUES
-(39, 'super.alexx@yahoo.com', 'parolamea', 'lexzor', 1, '5d1b0efa581485eb48718a2ab9b9447b', '[{\"id\":\"66\",\"tickets\":15}]');
+(39, 'super.alexx@yahoo.com', 'parolamea', 'lexzor', 1, '344124b82d9c7166d0e7a94a47b0e2b4', '[{\"id\":\"67\",\"tickets\":44},{\"id\":\"68\",\"tickets\":3}]');
 
 -- --------------------------------------------------------
 
@@ -59,14 +59,45 @@ CREATE TABLE `events` (
   `price` float NOT NULL,
   `description` text NOT NULL,
   `images` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `events`
 --
 
 INSERT INTO `events` (`id`, `title`, `start`, `end`, `max_tickets`, `price`, `description`, `images`) VALUES
-(67, 'BMW ROSU FA', '28-03-2023 00:10', '31-03-2023 23:10', '7000', 15, 'Descriere', '[\"events_images/1680037848/16800378480bmw-4-series-gran-coupe-onepager-sp-desktop.jpg.asset.1676973690941.jpg\"]');
+(67, 'BMW ROSU FA', '28-03-2023 00:10', '5-04-2023 23:10', '7000', 15, 'Descriere', '[\"events_images/1680037848/16800378480bmw-4-series-gran-coupe-onepager-sp-desktop.jpg.asset.1676973690941.jpg\"]'),
+(68, 'dasdsadsad', '31-03-2023 23:20', '5-04-2023 23:20', '1', 15, 'sadsadsadsadasdasdas', '[\"events_images/1680294019/16802940190Screenshot_6.png\"]');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices`
+--
+
+CREATE TABLE `invoices` (
+  `id` int(11) NOT NULL,
+  `firstName` text NOT NULL,
+  `lastName` text NOT NULL,
+  `phoneNumber` text NOT NULL,
+  `emailAddress` text NOT NULL,
+  `companyName` text NOT NULL,
+  `country` text NOT NULL,
+  `streetName` text NOT NULL,
+  `houseNumber` text NOT NULL,
+  `postCode` text NOT NULL,
+  `city` text NOT NULL,
+  `notes` text NOT NULL,
+  `products` text NOT NULL,
+  `paymentStatus` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `invoices`
+--
+
+INSERT INTO `invoices` (`id`, `firstName`, `lastName`, `phoneNumber`, `emailAddress`, `companyName`, `country`, `streetName`, `houseNumber`, `postCode`, `city`, `notes`, `products`, `paymentStatus`) VALUES
+(1, 'Pavel', 'Marin', '07949099021', 'pavel@eway-design.com', '', 'Romania', 'pl n cur', '2a', '12939', 'Baicoi', 'am pl mare', '[{\"id\":\"67\",\"title\":\"BMW ROSU FA\",\"quantity\":44,\"price\":660},{\"id\":\"68\",\"title\":\"dasdsadsad\",\"quantity\":3,\"price\":45}]', 'paid');
 
 -- --------------------------------------------------------
 
@@ -76,10 +107,19 @@ INSERT INTO `events` (`id`, `title`, `start`, `end`, `max_tickets`, `price`, `de
 
 CREATE TABLE `tickets` (
   `id` int(11) NOT NULL,
-  `uid` int(11) NOT NULL,
-  `eid` int(11) NOT NULL,
-  `bought_time` int(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `accountEmail` text NOT NULL,
+  `ticketQuantity` int(6) NOT NULL,
+  `eventId` int(11) NOT NULL,
+  `createdAt` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `accountEmail`, `ticketQuantity`, `eventId`, `createdAt`) VALUES
+(1, 'pavel@eway-design.com', 44, 67, '2023-04-02'),
+(2, 'pavel@eway-design.com', 3, 68, '2023-04-02');
 
 --
 -- Indexes for dumped tables
@@ -97,6 +137,12 @@ ALTER TABLE `accounts`
 -- Indexes for table `events`
 --
 ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invoices`
+--
+ALTER TABLE `invoices`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -119,13 +165,19 @@ ALTER TABLE `accounts`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+
+--
+-- AUTO_INCREMENT for table `invoices`
+--
+ALTER TABLE `invoices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
